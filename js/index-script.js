f41634098a562abf73d378360a829098adfb48e5
Christopher STEEN <csteen59@gmail.com>
2017-04-25 16:16:15 +0200

    change the dynamic background on index

--- js/index-script.js
+++ js/index-script.js
@@ -30,84 +30,100 @@ function background() {
 	var heurebg   = current.getHours();
 	switch (heurebg) {
 		case 0:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/00.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		case 1:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/01.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		case 2:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/02.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		case 3:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/03.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		case 4:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/04.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		case 5:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/05.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(175, 175, 175, 0.5)"
 			break;
 		case 6:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/06.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(175, 175, 175, 0.5)"
 			break;
 		case 7:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/07.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(175, 175, 175, 0.5)"
 			break;
 		case 8:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/08.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(175, 175, 175, 0.5)"
 			break;
 		case 9:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/09.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 10:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/10.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 11:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/11.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 12:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/12.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 13:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/13.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 14:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/14.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 15:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/15.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 16:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/16.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 17:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/17.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 18:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/18.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 			break;
 		case 19:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/19.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(187, 119, 17, 0.4)"
 			break;
 		case 20:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/20.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(187, 119, 17, 0.4)"
 			break;
 		case 21:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/21.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(187, 119, 17, 0.4)"
 			break;
 		case 22:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/22.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		case 23:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexbg/23.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0.8)"
 			break;
 		default:
-			document.getElementById('index-body').style.backgroundImage = "url(./img/indexback.png)"
+			document.getElementById('index-tone').style.backgroundColor = "rgba(0, 0, 0, 0)"
 	}
+}
+
+// text defil
+
+var position = 0;
+var msg = "La version 2 du CV est actuellement en construction, veuillez vous référer à la version 1 pour le moment.";
+var msg = "     " + msg;
+var long = msg.length;
+var fois = (80/msg.length) + 1;
+for(i = 0; i <= fois; i++) msg += msg;
+function textdefil() {
+document.defilform.deftext.value = msg.substring(position,position + 80);
+position++;
+if(position == long) position=0;
+setTimeout("textdefil()",100);
 }
 
 window.onload=function() {
 	background();
 	afficher();
+	textdefil();
 	setInterval(afficher,1000);
 }
